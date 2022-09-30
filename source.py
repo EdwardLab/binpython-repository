500db3f9fb68cd662cc422f9cc2c96259c9c8655
Edward Hsing <xingyujie50@gmail.com>
2022-09-30 20:04:12 +0800

    Update source.py

--- source.py
+++ source.py
@@ -1,7 +1,9 @@
 print("""
 BINPython software source switch
 1.Offical Github
-2.BINPython Offical US Server []
+2.BINPython Offical US Server
+
+***GitHub restricts large files and prevents abuse, so except for GitHub sources ("1" number), all support large files and third-party packages, we recommend switching non-GitHub sources
 """)
 num = input('Enter a sequence number to switch sources (eg "1"):')
 if num == "1":
