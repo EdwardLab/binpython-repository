87dcdcf4062915be8f0115c8a4718f1a49c094be
Edward Hsing <xingyujie50@gmail.com>
2022-09-30 21:26:01 +0800

    Update source.py

--- source.py
+++ source.py
@@ -9,16 +9,16 @@ BINPython software source switch
 num = input('Enter a sequence number to switch sources (eg "1"):')
 if num == "1":
     try:
-        os.makedirs(runpath + f'binpython_files/apps')
+        os.makedirs(runpath + f'/binpython_files/apps')
     except:
-        f = open(runpath + f"binpython_files/apps/source.config", "w")
+        f = open(runpath + f"/binpython_files/apps/source.config", "w")
         f.write("https://raw.githubusercontent.com/xingyujie/binpython-repository/main/")
     print("Done!")
 if num == "2":
     try:
-        os.makedirs(runpath + f'binpython_files/apps')
+        os.makedirs(runpath + f'/binpython_files/apps')
     except:
-        f = open(runpath + f"binpython_files/apps/source.config", "w")
+        f = open(runpath + f"/binpython_files/apps/source.config", "w")
         f.write("http://repo.binpython.org/")
     print("Done!")
 else:
