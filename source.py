6c34c2d891fa0b05b3fdb4841ce74670f62d01f4
Edward Hsing <xingyujie50@gmail.com>
2023-04-05 18:59:06 +0800

    Update source.py

--- source.py
+++ source.py
@@ -2,8 +2,7 @@ import os
 print("""
 BINPython software source switch
 1.Offical Github
-2.BINPython Offical US Server
-3.BINPython Offical CN Server
+2.BINPython Official US Server
 
 ***GitHub restricts large files and prevents abuse, so except for GitHub sources ("1" number), all support large files and third-party packages, we recommend switching non-GitHub sources
 """)
@@ -23,14 +22,6 @@ if num == "2":
         pass
     f = open(runpath + f"/binpython_files/apps/source.config", "w")
     f.write("http://repo.binpython.org/")
-    print("Done!")
-if num == "3":
-    try:
-        os.makedirs(runpath + f'/binpython_files/apps')
-    except:
-        pass
-    f = open(runpath + f"/binpython_files/apps/source.config", "w")
-    f.write("http://1.117.166.174:7800/")
     print("Done!")
 else:
     print('Please enter the correct number (eg "1")')
